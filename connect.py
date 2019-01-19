8ea5c142c14049087ba70a6bf9de4423cb9c6f27
Vik Chand <vikneshvar.chandrahasan@cgius.com>
2019-01-19 12:15:51 -0800

    initial commit

---
+++ connect.py
@@ -0,0 +1,12 @@
+msg = "Watsupp?"
+print("Hello World");
+print(msg);
+
+import twitter;
+api = twitter.Api(consumer_key="3Ch9xuxkykKkcZIE20gQgVEWp",
+                  consumer_secret="Y4hHXG11SbkLJhwKheOcNAt1X0NkZ0lHrSa3S7U39EaOBFzEWe",
+                  access_token_key="1028861674516705281-4QPOyJlsoaNioZoqj0CwrDggxyVH5I",
+                  access_token_secret="sieI6iuafzEExGDXzEh9wfnYIxd0LcV2QArSW2G7nq2fa");
+
+print(api.DEFAULT_CACHE_TIMEOUT);
+
