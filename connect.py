2d485b71d05ea27b85891cc08a4c612a48ff0235
Vik Chand <vikneshvar.chandrahasan@cgius.com>
2019-01-20 12:02:16 -0800

    updated connections

--- connect.py
+++ connect.py
@@ -1,12 +1,25 @@
-msg = "Watsupp?"
-print("Hello World");
-print(msg);
+# Import the necessary package to process data in JSON format
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
-import twitter;
-api = twitter.Api(consumer_key="3Ch9xuxkykKkcZIE20gQgVEWp",
-                  consumer_secret="Y4hHXG11SbkLJhwKheOcNAt1X0NkZ0lHrSa3S7U39EaOBFzEWe",
-                  access_token_key="1028861674516705281-4QPOyJlsoaNioZoqj0CwrDggxyVH5I",
-                  access_token_secret="sieI6iuafzEExGDXzEh9wfnYIxd0LcV2QArSW2G7nq2fa");
+# Import the tweepy library
+import tweepy
 
-print(api.DEFAULT_CACHE_TIMEOUT);
+consumer_key = "J5PXQdndY5uYURoH6HKjQVquO"
+consumer_secret = "XDprvdQhMIQu9VuhvTU0NEjCJMU9TouQ52vlStsY2gCkSdV5Yy"
+
+access_token = "1028861674516705281-N5Smpi61z57bExUy0ymDdUJVHURAAo"
+access_token_seceret = "mcDZYyeoHub83TxlVjFGS1FDRtLjypQ0TS08g1zsj9qhc"
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_seceret)
+
+# Create the api to connect to twitter with your creadentials
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True);
+
+for status in tweepy.Cursor(api.home_timeline).items(200):
+	print(status._json)
 
